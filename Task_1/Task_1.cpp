98ac2694c0c819bd77df923d421543cd2526f3a1
Andrei Dogadaev <dogadlev@yandex.ru>
2022-08-15 20:27:50 +0300

    Initial commit

---
+++ Task_1/Task_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russian");
+	std::string name;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!";
+	return 0;
+}
